ab2ff2695b1d7276f8b674ff11b0b2680be6b7fd
neeraj1909 <neeraj1909@gmail.com>
2017-10-28 18:14:38 +0530

    A list on a single line containing the cubes of the first N fibonacci numbers

---
+++ python-functional/map_and_lambda_function.py
@@ -0,0 +1,13 @@
+cube = lambda x: x**3 # complete the lambda function 
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    fibonacci_list =[0,1]
+    for i in range(2,n):
+        fibonacci_list.append(fibonacci_list[i-1] + fibonacci_list[i-2])
+    return fibonacci_list    
+
+
+
+if __name__ == '__main__':
+    n = int(input())
