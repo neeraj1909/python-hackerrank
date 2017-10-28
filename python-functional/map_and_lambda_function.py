f6d91b51a990757065079956289c6b8c05caf411
neeraj1909 <neeraj1909@gmail.com>
2017-10-28 19:07:40 +0530

    print N-1 lines as explained above

--- python-functional/map_and_lambda_function.py
+++ python-functional/map_and_lambda_function.py
@@ -5,9 +5,10 @@ def fibonacci(n):
     fibonacci_list =[0,1]
     for i in range(2,n):
         fibonacci_list.append(fibonacci_list[i-1] + fibonacci_list[i-2])
-    return fibonacci_list    
+    return fibonacci_list[0:n]   
 
 
 
 if __name__ == '__main__':
     n = int(input())
+    print(list(map(cube, fibonacci(n))))
