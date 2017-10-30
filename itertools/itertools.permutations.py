a4c924bc0453b342988cc2747e09eda135a3283d
neeraj1909 <neeraj1909@gmail.com>
2017-10-30 18:34:34 +0530

    print all possible permutations of size k of the string in lexicographic sorted order

--- itertools/itertools.permutations.py
+++ itertools/itertools.permutations.py
@@ -1,8 +1,12 @@
 from itertools import permutations
 
 def calculation(s, x):
+    """
+    for i in permutations(sorted(s), int(x)):
+        print("".join(i))
+    """
     print(*[''.join(i) for i in permutations(sorted(s), int(x))], sep = "\n")
-    
+ 
 
 def main():
     string , value = input().split()
