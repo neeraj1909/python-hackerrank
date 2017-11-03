963a70a3e85497593c83a621a8d77717f93e5ed1
neeraj1909 <neeraj1909@gmail.com>
2017-11-03 13:36:21 +0530

    perform integer divison and print a/b

---
+++ errors_and_exceptions/exceptions.py
@@ -0,0 +1,14 @@
+def main():
+    t = int(input())
+    for _ in range(t):
+        a, b = input().split()
+        try:
+            print(int(a) // int(b))
+        except ZeroDivisionError as e:
+            print("Error Code:",e)
+        except ValueError as e:
+            print("Error Code:",e)
+
+
+if __name__ == "__main__":
+    main()
